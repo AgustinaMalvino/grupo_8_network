1125ee38b73b96d94810ac54cf033b6dd3b87efa
AgustinaMalvino <agustinamalvino@gmail.com>
2022-12-22 16:45:09 -0300

    vistas login y register ok

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
