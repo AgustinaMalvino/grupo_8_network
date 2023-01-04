d8aa097b9b0b854fea5557de118834c9e9fb9ad8
fedeDiganchi <fede_diganchi@hotmail.com>
2023-01-03 23:55:05 -0300

    Sprint 3 - EJS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views')
 app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
@@ -10,21 +12,37 @@ console.log("Servidor corriendo en http://localhost:" + port)
 );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.render('index');
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.render('./users/login');
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.render('./users/register');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('./users/profile');
 });
 
 app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+    res.render('./users/productCart');
 });
 
 app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+    res.render('./products/productDetail');
+});
+
+app.get('/newProduct', (req, res) => {
+    res.render('./products/newProduct');
+});
+
+app.get('/updateProduct', (req, res) => {
+    res.render('./products/updateProduct');
+});
+
+app.get('/productList', (req, res) => {
+    res.render('./products/productList');
 });
